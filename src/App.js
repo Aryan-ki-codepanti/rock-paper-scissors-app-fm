782757d4d6cdc3d9c565504b4df10eab8b783014
Aryan Sethi <aryan@Aryans-Mac.local>
2024-07-24 13:34:10 +0530

    Added rule over

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Rules from "./Components/Rules";
 
 function App() {
     return (
         <div className="App">
             <Header />
+
+            <Rules />
         </div>
     );
 }
--- src/Components/Rules/index.jsx
+++ src/Components/Rules/index.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Rules.css";
+
+import crossImg from "../../Images/icon-close.svg";
+import rulesImg from "../../Images/image-rules.svg";
+
 const Rules = () => {
-    return <div>Rules</div>;
+    const [showRules, setShowRules] = useState(false);
+
+    return (
+        <div className="Rules container">
+            {!showRules && (
+                <button
+                    onClick={e => setShowRules(prev => true)}
+                    className="rule-btn"
+                >
+                    Rules
+                </button>
+            )}
+
+            {showRules && (
+                <div className="ruleBox">
+                    <div className="ruleModal">
+                        <div className="rules-header">
+                            <h1>Rules</h1>
+                            <div
+                                className="cross"
+                                onClick={e => setShowRules(prev => false)}
+                            >
+                                <img src={crossImg} alt="close-icon" />
+                            </div>
+                        </div>
+
+                        <div className="rules-body">
+                            <img src={rulesImg} alt="rules-pic" />
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Rules;
